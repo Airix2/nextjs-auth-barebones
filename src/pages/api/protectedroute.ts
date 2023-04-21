bf322c27033b302203d839772268df1f45525e67
Airix2 <alex.soto@cetys.edu.mx>
2023-04-21 14:01:06 -0700

    update

--- src/pages/api/protectedroute.ts
+++ src/pages/api/protectedroute.ts
@@ -7,21 +7,26 @@ export default async function handler(
 	req: NextApiRequest,
 	res: NextApiResponse
 ) {
-	const session = await getServerSession(req, res, authOptions);
+	try {
+		const session = await getServerSession(req, res, authOptions);
 
-	if (session) {
-		if (checkRole(session.user.role, req.url!)) {
-			return res.status(200).send({
-				content:
-					"This is protected content. You can access this content because you are signed in.",
+		if (session) {
+			if (checkRole(session.user.role, req.url!)) {
+				return res.status(200).send({
+					content:
+						"This is protected content. You can access this content because you are signed in.",
+				});
+			}
+			return res.status(403).send({
+				error: "You don't have the permission to view this route.",
+			});
+		} else {
+			res.status(401).send({
+				error: "You must be signed in to view the protected content on this page.",
 			});
 		}
-		return res.status(403).send({
-			error: "You don't have the permission to view this route.",
-		});
-	} else {
-		res.status(401).send({
-			error: "You must be signed in to view the protected content on this page.",
-		});
+	} catch (error) {
+		console.log("error", error);
+		res.status(500).send(error);
 	}
 }
